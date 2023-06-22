5bbdc9d5b956dc1ed556f01e0ca77f33d91160a0
nanana099 <nananabiz000@gmail.com>
2023-06-23 07:56:53 +0900

    make models

--- blog/views.py
+++ blog/views.py
@@ -7,3 +7,7 @@ def index(request):
 
 class IndexView(TemplateView):
     template_name = "blog/index.html"
+    def get(self, request, *args, **kwargs):
+        context = self.get_context_data(**kwargs)
+        print("IndexViewを使ってTOP画面を表示します！")
+        return self.render_to_response(context)
