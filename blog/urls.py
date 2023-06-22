32f15dc19f859e10084f5915f7e8b58309630b04
nanana099 <nananabiz000@gmail.com>
2023-06-23 07:34:27 +0900

    TemplateView

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import index
+from .views import index,IndexView
 
 urlpatterns = [
     path('', index, name="index"),
+    path('asview', IndexView.as_view(), name="indexview"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
 def index(request):
     #TOP画面を表示する関数
-    return render(request, 'index.html')
+    return render(request, 'blog/index.html')
+
+class IndexView(TemplateView):
+    template_name = "blog/index.html"
